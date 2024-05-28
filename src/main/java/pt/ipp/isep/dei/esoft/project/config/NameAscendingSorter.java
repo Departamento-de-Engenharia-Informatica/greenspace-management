886b113e593442c99250eb087ccd523ffb6e108d
eduedu15 <1231367@isep.ipp.pt>
2024-05-28 15:40:57 +0100

    JavaDocs for US27

--- src/main/java/pt/ipp/isep/dei/esoft/project/config/NameAscendingSorter.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/config/NameAscendingSorter.java
@@ -5,7 +5,17 @@ import pt.ipp.isep.dei.esoft.project.domain.GreenSpace;
 
 import java.util.List;
 
+/**
+ * Sorter implementation for sorting GreenSpace objects by name in ascending order.
+ */
 public class NameAscendingSorter implements GreenSpaceSorter {
+
+    /**
+     * Sorts the list of GreenSpace objects by name in ascending order.
+     *
+     * @param greenSpaces the list of GreenSpace objects to be sorted
+     * @return the sorted list of GreenSpace objects
+     */
     @Override
     public List<GreenSpace> sort(List<GreenSpace> greenSpaces) {
         greenSpaces.sort((gs1, gs2) -> gs1.getName().compareToIgnoreCase(gs2.getName()));
--- src/main/java/pt/ipp/isep/dei/esoft/project/config/SizeDescendingSorter.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/config/SizeDescendingSorter.java
@@ -5,7 +5,17 @@ import pt.ipp.isep.dei.esoft.project.domain.GreenSpace;
 
 import java.util.List;
 
+/**
+ * Sorter implementation for sorting GreenSpace objects by size in descending order.
+ */
 public class SizeDescendingSorter implements GreenSpaceSorter {
+
+    /**
+     * Sorts the list of GreenSpace objects by size in descending order.
+     *
+     * @param greenSpaces the list of GreenSpace objects to be sorted
+     * @return the sorted list of GreenSpace objects
+     */
     @Override
     public List<GreenSpace> sort(List<GreenSpace> greenSpaces) {
         greenSpaces.sort((gs1, gs2) -> Double.compare(gs2.getArea(), gs1.getArea()));
