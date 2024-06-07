8073057ba74b1090b82a4613e3493c095b71976a
eduedu15 <1231367@isep.ipp.pt>
2024-06-07 15:37:59 +0100

    javafx of US24 and 25 working

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/GUIController/ChangeStatusAgendaGUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/GUIController/ChangeStatusAgendaGUI.java
@@ -1,42 +1,67 @@
 package pt.ipp.isep.dei.esoft.project.application.controller.GUIController;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Button;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
+import pt.ipp.isep.dei.esoft.project.application.controller.AgendaController;
+import pt.ipp.isep.dei.esoft.project.domain.Agenda;
+import pt.ipp.isep.dei.esoft.project.ui.gui.ControllerWithEmail;
+import pt.ipp.isep.dei.esoft.project.ui.gui.GsmUIApplication;
+import pt.ipp.isep.dei.esoft.project.ui.gui.SceneSwitcher;
+
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 
-public class ChangeStatusAgendaGUI {
 
-    @FXML
-    private ComboBox<String> comboBox;
-
-    @FXML
-    private Button canceledButton;
+public class ChangeStatusAgendaGUI implements ControllerWithEmail {
 
     @FXML
-    private Button postponedButton;
+    private ChoiceBox<String> choiceBox;
+
+    private AgendaController agendaController;
+    private List<Agenda> agendaEntries;
+    private String userEmail;
+
 
     public void initialize() {
-        comboBox.getItems().addAll("Task 1", "Task 2", "Task 3"); // Example items, replace with actual task descriptions
+        setUserEmail(GsmUIApplication.getUserEmail());
+        agendaController = new AgendaController();
+        agendaEntries = agendaController.getAllAgendaEntries();
+
+        // Populate ChoiceBox
+        for (Agenda agenda : agendaEntries) {
+            choiceBox.getItems().add(agenda.getTaskDescription());
+        }
+    }
+    public void setUserEmail(String email) {
+        this.userEmail = email;
+        System.out.println("link 2"+userEmail);
     }
 
     @FXML
     private void handleCanceledStatus(ActionEvent event) {
-        String selectedTask = comboBox.getSelectionModel().getSelectedItem();
+        String selectedTask = choiceBox.getSelectionModel().getSelectedItem();
         if (selectedTask != null) {
-            // Perform the action to change the status to Canceled
-            System.out.println("Changing status of " + selectedTask + " to Canceled.");
-            // Add actual logic to change the status in your model/controller here
-            showAlert(Alert.AlertType.INFORMATION, "Status Changed", "Status of " + selectedTask + " changed to Canceled.");
+            Optional<Agenda> optionalAgenda = agendaEntries.stream()
+                    .filter(agenda -> agenda.getTaskDescription().equals(selectedTask))
+                    .findFirst();
+
+            if (optionalAgenda.isPresent()) {
+                Agenda agenda = optionalAgenda.get();
+                if (agenda.getStatus().equals("Canceled")) {
+                    showAlert(Alert.AlertType.WARNING, "Status Already Canceled", "The status of " + selectedTask + " is already Canceled.");
+                } else {
+                    agenda.setStatus("Canceled");
+                    agendaController.updateAgendaEntry(agenda);
+                    showAlert(Alert.AlertType.INFORMATION, "Status Changed", "Status of " + selectedTask + " changed to Canceled.");
+                }
+            }
         } else {
             showAlert(Alert.AlertType.WARNING, "No Task Selected", "Please select a task to change its status.");
         }
@@ -44,14 +69,35 @@ public class ChangeStatusAgendaGUI {
 
     @FXML
     private void handlePostponedStatus(ActionEvent event) {
-        String selectedTask = comboBox.getSelectionModel().getSelectedItem();
+        String selectedTask = choiceBox.getSelectionModel().getSelectedItem();
         if (selectedTask != null) {
-            // Perform the action to change the status to Postponed
-            System.out.println("Changing status of " + selectedTask + " to Postponed.");
-            // Add actual logic to change the status in your model/controller here
-            showAlert(Alert.AlertType.INFORMATION, "Status Changed", "Status of " + selectedTask + " changed to Postponed.");
+            Optional<Agenda> optionalAgenda = agendaEntries.stream()
+                    .filter(agenda -> agenda.getTaskDescription().equals(selectedTask))
+                    .findFirst();
+
+            if (optionalAgenda.isPresent()) {
+                Agenda agenda = optionalAgenda.get();
+                if (agenda.getStatus().equals("Postponed")) {
+                    showAlert(Alert.AlertType.WARNING, "Status Already Postponed", "The status of " + selectedTask + " is already Postponed.");
+                } else {
+                    agenda.setStatus("Postponed");
+                    agendaController.updateAgendaEntry(agenda);
+                    showAlert(Alert.AlertType.INFORMATION, "Status Changed", "Status of " + selectedTask + " changed to Postponed.");
+                }
+            }
         } else {
             showAlert(Alert.AlertType.WARNING, "No Task Selected", "Please select a task to change its status.");
+        }
+    }
+
+    @FXML
+    public void handleBack(javafx.event.ActionEvent actionEvent) {
+
+        try {
+            SceneSwitcher.switchToScene("/fxml/GsmUIMenu.fxml", "Register Green Space", (Node) actionEvent.getSource(), userEmail);
+
+        } catch (IOException e) {
+            showAlert(Alert.AlertType.ERROR, "Error", "Failed to load the Main Menu.");
         }
     }
 
@@ -62,6 +108,4 @@ public class ChangeStatusAgendaGUI {
         alert.setContentText(message);
         alert.showAndWait();
     }
-
-
 }
