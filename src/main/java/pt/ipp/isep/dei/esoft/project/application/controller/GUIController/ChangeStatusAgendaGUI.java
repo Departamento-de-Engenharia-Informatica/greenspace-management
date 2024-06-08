bd1b970799a67fb4b87011297e2fd007a511d32e
eduedu15 <1231367@isep.ipp.pt>
2024-06-08 15:46:50 +0100

    correction of javafx US24

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/GUIController/ChangeStatusAgendaGUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/GUIController/ChangeStatusAgendaGUI.java
@@ -3,8 +3,7 @@ package pt.ipp.isep.dei.esoft.project.application.controller.GUIController;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 import pt.ipp.isep.dei.esoft.project.application.controller.AgendaController;
 import pt.ipp.isep.dei.esoft.project.domain.Agenda;
 import pt.ipp.isep.dei.esoft.project.ui.gui.ControllerWithEmail;
@@ -14,20 +13,29 @@ import pt.ipp.isep.dei.esoft.project.ui.gui.SceneSwitcher;
 import javafx.scene.Node;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
-
 
 public class ChangeStatusAgendaGUI implements ControllerWithEmail {
 
     @FXML
     private ChoiceBox<String> choiceBox;
 
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Button canceledbtn;
+    @FXML
+    private Button postponedbtn;
+    @FXML
+    private Button backbtn;
+    @FXML
+    private Label datelbl;
+
     private AgendaController agendaController;
     private List<Agenda> agendaEntries;
     private String userEmail;
-
 
     public void initialize() {
         setUserEmail(GsmUIApplication.getUserEmail());
@@ -39,9 +47,10 @@ public class ChangeStatusAgendaGUI implements ControllerWithEmail {
             choiceBox.getItems().add(agenda.getTaskDescription());
         }
     }
+
     public void setUserEmail(String email) {
         this.userEmail = email;
-        System.out.println("link 2"+userEmail);
+        System.out.println("link 2" + userEmail);
     }
 
     @FXML
@@ -77,13 +86,29 @@ public class ChangeStatusAgendaGUI implements ControllerWithEmail {
 
             if (optionalAgenda.isPresent()) {
                 Agenda agenda = optionalAgenda.get();
-                if (agenda.getStatus().equals("Postponed")) {
-                    showAlert(Alert.AlertType.WARNING, "Status Already Postponed", "The status of " + selectedTask + " is already Postponed.");
-                } else {
-                    agenda.setStatus("Postponed");
-                    agendaController.updateAgendaEntry(agenda);
-                    showAlert(Alert.AlertType.INFORMATION, "Status Changed", "Status of " + selectedTask + " changed to Postponed.");
-                }
+                datePicker.setVisible(true);
+                canceledbtn.setVisible(false);
+                postponedbtn.setVisible(false);
+                backbtn.setVisible(false);
+                datelbl.setVisible(true);
+                choiceBox.setDisable(true);
+                datePicker.setOnAction(e -> {
+                    LocalDate newDate = datePicker.getValue();
+                    if (newDate != null && newDate.isAfter(agenda.getExpectedDate())) {
+                        agenda.setExpectedDate(newDate);
+                        agenda.setStatus("Postponed");
+                        agendaController.updateAgendaEntry(agenda);
+                        showAlert(Alert.AlertType.INFORMATION, "Status Changed", "Status of " + selectedTask + " changed to Postponed.");
+                        datePicker.setVisible(false);
+                        postponedbtn.setVisible(true);
+                        canceledbtn.setVisible(true);
+                        backbtn.setVisible(true);
+                        datelbl.setVisible(false);
+                        choiceBox.setDisable(false);
+                    } else {
+                        showAlert(Alert.AlertType.WARNING, "Invalid Date", "Please select a date after the current task date.");
+                    }
+                });
             }
         } else {
             showAlert(Alert.AlertType.WARNING, "No Task Selected", "Please select a task to change its status.");
@@ -92,10 +117,8 @@ public class ChangeStatusAgendaGUI implements ControllerWithEmail {
 
     @FXML
     public void handleBack(javafx.event.ActionEvent actionEvent) {
-
         try {
             SceneSwitcher.switchToScene("/fxml/GsmUIMenu.fxml", "Register Green Space", (Node) actionEvent.getSource(), userEmail);
-
         } catch (IOException e) {
             showAlert(Alert.AlertType.ERROR, "Error", "Failed to load the Main Menu.");
         }
